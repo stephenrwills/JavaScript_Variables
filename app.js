6bef09852fe72e9769480219ff11844c08477e5d
Stephen Wills <stephenrwills94@gmail.com>
2022-08-08 14:48:24 -0500

    finished all exercises

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Stephen";
+let lastName = "Wills";
+var age = 27;
 
-// Exercise 2
+let language = "JavaScript";
+let createdYear = "1995";
+let isCaseSensitive = true;
 
-// Exercise 3
+console.log(`${language} was first released in ${createdYear}`);
+
+let price = 19.99
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+
+
